a23fa4240cde38f8d060faa89f2c110378d2e6a9
rysette12 <39281674+rysette12@users.noreply.github.com>
2018-12-09 13:40:00 -0500

    NavigationDrawer avec BaseActivity permettant de réutiliser le code de création du tiroir dans les autres activités.

--- app/src/main/java/com/example/maryse/laboratoire3/Acivity2.java
+++ app/src/main/java/com/example/maryse/laboratoire3/Acivity2.java
@@ -1,8 +1,10 @@
 package com.example.maryse.laboratoire3;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends BaseActivity {
+public class Acivity2 extends MainActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
